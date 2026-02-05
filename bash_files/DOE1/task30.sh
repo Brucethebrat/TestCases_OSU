780d23f322215ec62aa8bb3ec1f929ab341e317d
Bruce Cheng <brucecheng2000@gmail.com>
2026-02-05 14:33:59 -0500

    DOE2 test cases & bash files
    - Some new levels for test cases, also removed some
    - bash files for DOE2, with the model factors

---
+++ bash_files/DOE1/task30.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd /users/PAS2414/brucecheng/DOE1
+
+SCRIPT_PATH='run_scheduler_with1010_4OSC_grb_lic_include.py'
+
+. DOE1_env/bin/activate
+module load gurobi/12.0.0
+
+python $SCRIPT_PATH 30 3 20 120
---
+++ bash_files/DOE2/task10.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd /users/PAS2414/brucecheng/DOE2
+
+SCRIPT_PATH='run_scheduler_with1107_4OSC_grb_lic_include.py'
+
+. DOE2_env/bin/activate
+# module load gurobi/12.0.0
+
+python $SCRIPT_PATH 10 3 20 30 400 400 -40 30 -20 -60 1
